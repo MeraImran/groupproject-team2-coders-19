8fd221cdc6371ed1376fdf1d9bb1f433a514f6fe
Dalton <d_coppage@u.pacific.edu>
2020-11-13 11:07:36 -0800

    "Pushing unfinished GamePane"

--- group-project/src/starter/GamePane.java
+++ group-project/src/starter/GamePane.java
@@ -1,6 +1,11 @@
 package starter;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Rectangle;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.awt.event.MouseEvent;
 import java.util.ArrayList;
 import javax.swing.Timer;
@@ -9,7 +14,7 @@ import acm.graphics.GImage;
 import acm.graphics.GObject;
 import acm.graphics.GOval;
 
-public class GamePane extends GraphicsPane implements ActionListener {
+public class GamePane extends GraphicsPane implements ActionListener, KeyListener {
 	private MainApplication program; // you will use program to get access to
 										// all of the GraphicsProgram calls
 	
@@ -23,14 +28,23 @@ public class GamePane extends GraphicsPane implements ActionListener {
 	private int xVelocity = 50;
 	private int yVelocity = 0;
 	
-
+	private Spaceship ship;
+	double x = 0, y = 0, velx = 0, vely = 0;
+	int xPos, yPos;
+	
 	public GamePane(MainApplication app) {
 		this.program = app;
 		drawAliens();
+		drawSpaceship();
 		Timer someTimer = new Timer(1000, this);
 		someTimer.start();
+		addKeyListener(this);
+		setFocusTraversalKeysEnabled(false);
 	}
-	
+
+	private void setFocusTraversalKeysEnabled(boolean b) {} // TODO Auto-generated method stub
+	private void addKeyListener(GamePane gamePane) {} // TODO Auto-generated method stub
+
 	private boolean outOfBounds() {
 		for (int i = 0; i < rowAliens; i++) {
 			for (int j = 0; j < colAliens; j++) {
@@ -87,8 +101,33 @@ public class GamePane extends GraphicsPane implements ActionListener {
 		//program.remove(para);
 	}
 
-	@Override
-	public void mousePressed(MouseEvent e) { //Commands for spaceship will go here
-		
+	
+	//Where Spaceship and Bullet Start
+	public void drawSpaceship() {
+		ship = new Spaceship(xPos, yPos);
 	}
+	
+	public void actionPerformed2(ActionEvent e) {
+		x += velx;
+		y += vely;
+	}
+	public void left() {
+		velx = -1.5;
+		vely = 0;
+	}
+	public void right() {
+		velx = 0;
+		vely = 1.5;
+	}
+	public void keyPressed(KeyEvent e) {
+		int code = e.getKeyCode();
+		if (code == KeyEvent.VK_LEFT) {
+			left();
+		}
+		if (code == KeyEvent.VK_RIGHT) {
+			right();
+		}
+	}
+	public void keyTyped(KeyEvent e) {}
+	public void keyReleased(KeyEvent e) {}
 }
